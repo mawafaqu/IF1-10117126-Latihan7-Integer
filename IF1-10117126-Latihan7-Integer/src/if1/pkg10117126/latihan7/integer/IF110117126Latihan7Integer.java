4d3c1865162a85b4412ea67c4200afc173a2c746
mawafaqu <63239367+mawafaqu@users.noreply.github.com>
2020-10-21 00:44:25 +0700

    Add files via upload

---
+++ IF1-10117126-Latihan7-Integer/src/if1/pkg10117126/latihan7/integer/IF110117126Latihan7Integer.java
@@ -0,0 +1,37 @@
+/*
+ * 
+ * @author
+ * NAMA : MAWA FAQU ROCHMAN
+ * KELAS : PBO1
+ * NIM : 10117126
+ * Deskripsi Program : 
+*/
+
+package if1.pkg10117126.latihan7.integer;
+
+/**
+ *
+ * @author MAWA
+ */
+public class IF110117126Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 900000000000L;
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+    }
+
+}
